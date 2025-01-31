73c9a72a0aaf268eb2a2be15c9d722c3157df3ff
vivekmishra22 <vivekunofficialmail.com>
2025-01-31 15:34:02 +0530

    explanation comment added to each line

--- Controller.js
+++ Controller.js
@@ -1,73 +1,63 @@
-const model = require('./Model');
+// Import the model (from the Model.js file) which is used to interact with the database.
+const model = require('./Model');   
 
-// GET API (Fetch All Data)
-
+// GET API ( fetch all data from the database )
 const getdata = async (req, res) => {
     try {
-        const data = await model.find()
-        res.status(200).send({ data })
+
+        // Fetch all records from the database using the 'find' method, await keyword wait util promises resolved or reject
+        const data = await model.find()  
+        // Send the fetched data as a response with a 200 status code
+        res.status(200).send({ data })  
+
     } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message : 'internal server error'})
+
+        // Log the error to the console for debugging
+        console.log(error);     
+        // If an error occurs during saving, log it and send a 500 internal server error response
+        // Send a 500 status code with an error message if something goes wrong, 
+        return res.status(500).json({ message: 'internal server error' })       
+
     }
 }
 
-// const getdata = async (req, res) => {
-//     try {
-//         const data = await model.find();
-//         if (!data.length) {
-//             return res.status(404).json({ message: 'No records found' });
-//         }
-//         res.status(200).json({ data });
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).json({ message: 'Internal server error' });
-//     }
-// };
 
-
-// GET API ONLY FIND ONE
+// GET API ONLY FIND ONE ( fetch a one user's data based on the user ID )
 const GetuserById = async (req, res) => {
     try {
+
+        // Extract the `_id` parameter from the request URL
         const { _id } = req.params
+        // Fetch user data from the database by matching the '_id'
         const userData = await model.findOne({ _id: _id })
+        // Send the fetched document as a response with a 200 status code
         res.status(200).send({ userData })
 
     } catch (err) {
-        // res.status(400).send(err)
         console.log(err)
     }
 }
 
-// const GetuserById = async (req, res) => {
-//     try {
-//         const { _id } = req.params;
-//         if (!_id) {
-//             return res.status(400).json({ message: 'ID is required' });
-//         }
-//         const userData = await model.findOne({ _id });
-//         if (!userData) {
-//             return res.status(404).json({ message: 'User not found' });
-//         }
-//         res.status(200).json({ userData });
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ message: 'Internal server error' });
-//     }
-// };
 
-
-// post api
-
+// POST API (Add New Data)
 
 const add = async (req, res) => {
+
+    // Extract `ctitle` and `ctext` from the request body
     const { ctitle, ctext } = req.body;
+    
     try {
+
+        // Create a new document using the Mongoose model
         const data = new model({
-            ctitle, ctext, 
-            image:req.file.filename
+            ctitle, ctext,
+            // Extract the filename of the uploaded file, this stores the filename of the uploaded image
+            image: req.file.filename        
         });
+
+        // Save the new document to the MongoDB collection
         const userdata = await data.save()
+        // Send the saved document as a response
         res.send({ userdata });
     }
     catch (error) {
@@ -76,46 +66,57 @@ const add = async (req, res) => {
     }
 }
 
-// Delete api
+// DELETE API (Delete Data by ID)
 
 const deletedata = async (req, res) => {
     try {
+        
+        // Use the ID from the URL params(parameters) to Delete a document from the database
         const data = await model.deleteOne({ _id: req.params.id })
+        // Send the deletion result as a response with a 200 status code
         res.status(200).send({ data })
     } catch (error) {
+        // If an error occurs, send back a 500 error response
         res.status(500).send(error)
     }
 }
 
 
-// Update api
+// PUT API (Update Data by ID)
 
 const putdata = async (req, res) => {
+
+    // Extract `ctitle` and `ctext` from the request body
     const { ctitle, ctext } = req.body;
     try {
+
+        // Update a document in the collection by matching its ID (`_id`) from the request parameters(from the URL params)
         const data = await model.updateOne(
+
+            { _id: req.params._id },            // Find the course by its '_id'
             {
-                _id: req.params._id
-            },
-            {
-                $set:{
-                    ctitle,
-                    ctext,
-                    image:req.file.filename
+                $set: {
+                    ctitle,                     // Update `ctitle`
+                    ctext,                      // Update `ctext`
+                    image: req.file.filename    // Update `image` with the new filename
                 }
             },
         );
-        if(data) {
-            res.status(200).send({ message : "Data Updated found" });
+        if (data) {
+            // If the document is updated, send a success message
+            res.status(200).send({ message: "Data Updated found" });
         } else {
-            res.status(404).send({ message : "User not found" });
+            // If no document is found, send a 404 status code with a message
+            res.status(404).send({ message: "User not found" });
         }
     } catch (error) {
+
+        // Log the error to the console for debugging
         console.log(error);
-        res.status(404).send({ message : "internal Server Error" });
+        // Send a 404 status code with an error message ("internal Server Error")
+        res.status(404).send({ message: "internal Server Error" });
     }
 }
 
-
-
+// Export all the functions(API handler functions) to be used in other files, 
 module.exports = { getdata, add, GetuserById, deletedata, putdata };
--- Model.js
+++ Model.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose')    // Import the Mongoose library to interact with MongoDB
 
-const Course = mongoose.Schema({ 
-    ctitle:String, 
-    ctext:String, 
-    image:String
+const Course = mongoose.Schema({  // Defining the schema for the "Course" model, A schema defines the structure of documents in a collection
+
+    ctitle:String,  // Define a field `ctitle` of type String to store the course title
+    ctext:String,   // Define a field `ctext` of type String to store the course description or text
+    image:String    // Define a field `image` of type String to store the URL or path of the course image
  })
 
 module.exports = mongoose.model('mern', Course);
+// Export the Mongoose model for the "Course" schema
+// The model is named "mern" and will correspond to a collection named "merns" in MongoDB
+// (Mongoose automatically pluralizes the model name to create the collection name)
 
 
 
